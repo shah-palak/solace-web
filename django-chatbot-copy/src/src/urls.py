d28997c979cbde1ffbd30c014afe95fdc278ca08
shah-palak <110431679+shah-palak@users.noreply.github.com>
2023-11-02 10:01:55 -0700

    urls.py

--- django-chatbot-copy/src/src/urls.py
+++ django-chatbot-copy/src/src/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from demo import views
+from src.demo import views
 
 urlpatterns = [
     path('', views.title),
